07a5db443059e35cafd7b926b23bd714d632500e
Masi <villalbamasiel@gmail.com>
2021-04-03 10:13:27 -0400

    header

--- public/assets/js/header.js
+++ public/assets/js/header.js
@@ -7,3 +7,15 @@ $('.losPagos i').each(function(index, element){
     var selfWidth = $(this).width
     $(this).css('left', (tdWidth-selfWidth)/2)
 });
+
+var resize_mastercard = function(){
+    var visa_width = $('#mastercard_img').next('img').width()
+    var visa_heigth = $('#mastercard_img').next('img').height()
+    console.log('visa_width: ', visa_width)
+    console.log('visa_heigth: ', visa_heigth)
+    $('#mastercard_img').css('width', visa_width)
+    $('#mastercard_img').css('height', visa_heigth)
+  };
+
+$(window).resize(resize_mastercard);
+$(window).bind('afterunload', resize_mastercard);
