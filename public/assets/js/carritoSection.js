ddf9204cafb755d3d8c970746dc202b8a554a12e
Masi <villalbamasiel@gmail.com>
2021-04-18 21:57:32 -0400

    More

--- public/assets/js/carritoSection.js
+++ public/assets/js/carritoSection.js
@@ -1,6 +1,15 @@
 var resize_all = function(){
     var parentWidth = $('#carritoSection').width()
     $('#cant_sub div').css('width', (parentWidth - 10)/ 2)
+
+    /*
+    var value = (parentWidth/3) + (parentWidth/10)
+    if(value < 60){
+        $('.cantidad_texto').css('font-size', '40%')
+        $('#_0000_00_€').css('font-size', '48%')
+        $('#_1').css('font-size', '48%')
+    } 
+    */
   };
 
 $(window).resize(resize_all);
